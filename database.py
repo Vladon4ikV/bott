764147f62f1b0faccb56bc0dae543a53c821fab2
Vladon4ikV <120712116+Vladon4ikV@users.noreply.github.com>
2026-07-05 18:50:34 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
